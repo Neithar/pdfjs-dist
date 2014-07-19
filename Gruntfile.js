679a267523f09d89efe3a3bbc0782124aef0e007
Fery Wardiyanto <ferywardiyanto@gmail.com>
2014-07-19 16:14:23 +0700

    Update some files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,10 @@
 
 module.exports = function (grunt) {
 
-  require('load-grunt-tasks')(grunt);
-  
-  var self = {
+  require('load-grunt-tasks')(grunt);;
+
+  grunt.initConfig({
+
     execute: {
       generic: {
         options: {
@@ -15,24 +16,30 @@ module.exports = function (grunt) {
         src: ['pdfjs/make.js']
       }
     },
+
     copy: {
       pdfjs: {
         files: [
           // includes files within path
-          { expand: true, flatten: true, src: ['pdfjs/build/pdf.js', 'pdfjs/build/pdf.worker.js', 'pdfjs/build/generic/web/compatibility.js'], dest: 'dist/', filter: 'isFile'},
+          {
+            expand: true,
+            flatten: true,
+            dest: 'dist/',
+            filter: 'isFile',
+            src: [
+              'pdfjs/build/pdf.js',
+              'pdfjs/build/pdf.worker.js',
+              'pdfjs/build/generic/web/compatibility.js'
+            ]
+          }
         ]
       }
     }
-  };
 
-  grunt.initConfig(self)
+  });
 
-  grunt.registerTask('build', [
-    'execute', 'copy'
-  ]);
+  grunt.registerTask('build', [ 'execute', 'copy' ]);
 
-  grunt.registerTask('default', [
-    'build'
-  ]);
+  grunt.registerTask('default', [ 'build' ]);
 
 };
